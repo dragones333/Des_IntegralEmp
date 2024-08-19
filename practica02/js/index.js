92e448bd522daa13a897a651822cd9bd2641fbbd
unknown <orlandoabta@gmail.com>
2024-08-19 09:37:32 -0600

    practica02

---
+++ practica02/js/index.js
@@ -0,0 +1,10 @@
+var miNumero = 10;
+miNumero = 10 + 10;
+miNumero = 2.5;
+console.log(miNumero);
+var booleano = true;
+booleano = false;
+//
+//
+var encendido;
+console.log(encendido);
---
+++ practica02/src/index.ts
@@ -0,0 +1,13 @@
+let miNumero:number =10;
+
+miNumero=10+10;
+miNumero=2.5;
+console.log(miNumero);
+
+let booleano: boolean=true;
+booleano=false;
+
+// booleano=3;
+//
+let encendido:boolean;
+//console.log(encendido);
