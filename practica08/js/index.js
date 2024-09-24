c1c47186f2d87eec3a9c2f579a5299ef451ba8be
unknown <orlandoabta@gmail.com>
2024-09-24 08:22:56 -0600

    pardkke

--- practica08/js/index.js
+++ practica08/js/index.js
@@ -1,31 +1,25 @@
+"use strict";
 console.log("Hola codespace");
-
-const nuevoHeroe:string ='Chapulin Colorado';
-
-function returName():string{
+const nuevoHeroe = 'Chapulin Colorado';
+function returName() {
     return nuevoHeroe;
 }
 console.log(returName());
-
-
 const heroeNuevo = returName();
-let otraVariable=returName();
+let otraVariable = returName();
 console.log(heroeNuevo);
-console.log('De quetipo es:', typeof otraVariable)
-
+console.log('De quetipo es:', typeof otraVariable);
 //Parametros obligatorios en funciones
-const nombreCompleto =(nombre:string,apellido: string)=>{
+const nombreCompleto = (nombre, apellido) => {
     return `${nombre} ${apellido}`;
-}
-const tuNombre=nombreCompleto("Carlos","Serrano")
+};
+const tuNombre = nombreCompleto("Carlos", "Serrano");
 console.log(tuNombre);
-
 //Parametros opcionales en funciones, swith
-const nombreCompleto2 =(nombre:string,apellido?: string)=>{
+const nombreCompleto2 = (nombre, apellido) => {
     return `${nombre} ${apellido || 'No se proporciono apellido'}`;
-
-}
-const tuNombre2=nombreCompleto2("Carlos2")
+};
+const tuNombre2 = nombreCompleto2("Carlos2");
 console.log(tuNombre2);
 /*const nombreCompleto3 = (nombre?: string, apellido?: string, mayusculas: boolean = false) => {
     return mayusculas ? `${nombre || 'No se proporciono nombre'} ${apellido || 'No se proporciono apellido'}`;
@@ -33,11 +27,9 @@ console.log(tuNombre2);
 };
 const tuNombre3 = nombreCompleto3("Carlos3", "Serrano 3", true);
 console.log(tuNombre3);*/
-
 //Operador REST
-const nombreCompleto4 = (nombre:string,edad:Number,...restoDeApellidos:string[])=>{
-return `${nombre}...RESTO ${restoDeApellidos.join(' ')} ${edad}`
-}
-
-const superman4=nombreCompleto4('Clark',32,'Kent','Oswald','Smith');
+const nombreCompleto4 = (nombre, edad, ...restoDeApellidos) => {
+    return `${nombre}...RESTO ${restoDeApellidos.join(' ')} ${edad}`;
+};
+const superman4 = nombreCompleto4('Clark', 32, 'Kent', 'Oswald', 'Smith');
 console.log(superman4);
